182a16ff3d961f56bc611567754ae6f391dc2642
Yinan Fang <yinan_fang@hotmail.com>
2015-11-11 23:43:04 -0500

    test...

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ var http = require("http");
 
 http.createServer(function(request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("Hello World. It's different. Merged with Github");
+  response.write("Hello World. It's different. Merged with Github!!!");
   response.end();
 }).listen(28096);
