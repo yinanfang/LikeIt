18ad910ec3fd40a4ba4a4b4cf2ba3fce2f83d24e
Yinan Fang <yinan_fang@hotmail.com>
2015-11-15 00:27:04 -0500

    Fix test script port problem and kill it after finish

--- test/api/api_spec.js
+++ test/api/api_spec.js
@@ -1,9 +1,10 @@
 'use strict';
 
 var frisby = require('frisby');
+var appConfig = require('../../config/config.js');
 
 frisby.create('Ensure we are dealing with a teapot')
-  .get('http://localhost:4000')
+  .get('http://localhost:'+appConfig.port.app.main)
     .expectStatus(200)
 .toss();
 
